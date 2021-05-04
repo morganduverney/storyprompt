d8ea0309b02dd73c77c57e7d66433dde962cff8b
Morgan Duverney <morgan.duverney@gmail.com>
2021-05-04 11:05:56 -0400

    Initial Commit

---
+++ StoryPrompt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryPrompt
+//
+//  Created by Morgan Edmonds on 5/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
